46476af5ee4e313193141d63b6819177010e634f
Shabarish Ramaswamy <shabarishramaswamy@gmail.com>
2025-12-06 22:32:31 -0800

    Made the app run on python3 main.py rather than the annoying uvicorn run thing

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import src.router
 
 app = FastAPI()
+PORT = 8000
 
 app.include_router(src.router.Router)
-app.mount("/", StaticFiles(directory="frontend-build", html = True), name="site")
+app.mount("/", StaticFiles(directory="backend/frontend-build", html = True), name="site")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=True)
